4d74a3aa35047bc72a6be574fd6c8bbd5374fa66
Nathan Buskirk <nbuskirk@gmail.com>
2016-06-21 22:54:22 -0700

    fixes

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import {render} from 'react-dom';
+/* Import React Base */
+import React from 'react'
+/* Borrow render() from react-dom */
+import {render} from 'react-dom'
 
-class App extends React.Component {
+/* Create a new object taking ONLY the React stuff we want */
+export class App1 extends React.Component {
+  constructor(props){
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+  onClick(e){
+    console.log(this.props.name)
+  }
   render () {
-    return <p> Hello React!</p>;
+    return (
+        <div onClick={this.onClick.bind(this)}>
+          <p> Hello {this.props.name}</p>
+        </div>
+    )
   }
 }
 
-render(<App/>, document.getElementById('app'));
+/* Create a new object using all React properties */
+var App2 = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <p> Hello {this.props.name}</p>
+      </div>
+    )
+  }
+})
+
+/* Render the application to the page node */
+var appNode = document.getElementById('app-root')
+render(<div><App1 name="App1" /><App2 name="App2"/></div>, appNode)
